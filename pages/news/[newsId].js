4abad128fd415c24578ebc70beb7bdfdc0aa099b
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-12-11 15:31:10 -0300

    extracting dynamic parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const DetailPage = () => {
+  const router = useRouter();
+
+  const { newsId } = router.query;
+
   return (
     <div>
-      <h1>The Detail Page</h1>
+      <h1>The Detail Page: {newsId}</h1>
     </div>
   );
 };
