20abc6c89674ad0bb823062742f521783c85a10b
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-12-11 15:38:55 -0300

    linking between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
 
 const NewPage = () => {
   return (
-    <div>
+    <>
       <h1>The News Page</h1>
-    </div>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a great framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something else</Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
